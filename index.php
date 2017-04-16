f48484342f31aac788201d7b2e33c15396567dee
Miguel Araya <miaraya@gmail.com>
2017-04-16 12:59:10 +0700

    v1.0

---
+++ index.php
@@ -0,0 +1,454 @@
+<!DOCTYPE html>
+<html dir="ltr" lang="en-US">
+<head>
+
+	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+	<meta name="author" content="Miguel Araya" />
+	<meta name="description" content="Professional Aerial Imagery">
+
+	<!-- Stylesheets
+	============================================= -->
+	<link href="http://fonts.googleapis.com/css?family=Lato:300,400,400italic,600,700|Montserrat:400,700|Merriweather" rel="stylesheet" type="text/css" />
+	<link rel="stylesheet" href="css/bootstrap.css" type="text/css" />
+	<link rel="stylesheet" href="style.css" type="text/css" />
+	<link rel="stylesheet" href="css/dark.css" type="text/css" />
+
+	<!-- Specific Stylesheet -->
+	<link rel="stylesheet" href="demos/media-agency/media-agency.css" type="text/css" />
+	<!-- / -->
+
+	<link rel="stylesheet" href="css/font-icons.css" type="text/css" />
+	<link rel="stylesheet" href="one-page/css/et-line.css" type="text/css" />
+	<link rel="stylesheet" href="css/animate.css" type="text/css" />
+	<link rel="stylesheet" href="css/magnific-popup.css" type="text/css" />
+
+	<link rel="stylesheet" href="demos/media-agency/fonts.css" type="text/css" />
+<link rel='shortcut icon' type='image/x-icon' href='/favicon.ico' />
+
+	<link rel="stylesheet" href="css/responsive.css" type="text/css" />
+	<style type="text/css">
+		@font-face {
+		    font-family: "Neuropolitical";
+		    src: url(./fonts/neuropolitical.ttf) format("truetype");
+		}
+		.customfont { 
+		    font-family: "Neuropolitical", Verdana, Tahoma;
+		    color: #860813;
+		}
+	</style>
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+
+	<!--[if lt IE 9]>
+		<script src="http://css3-mediaqueries-js.googlecode.com/svn/trunk/css3-mediaqueries.js"></script>
+	<![endif]-->
+
+	<!-- Document Title
+	============================================= -->
+	<title>Ascent UAV Limited - Professional Aerial Imagery</title>
+
+</head>
+
+<body class="" data-loader="14">
+
+	<div class="side-cover-wrapper full-screen visible-lg visible-md">
+		<div class="fslider" data-speed="3000" data-pause="5000" data-animation="fade" data-arrows="false" data-pagi="false" style="position: absolute; width: 100%; height: 100%; top: 0; left: 0; background-color: #333;">
+			<div class="flexslider" style="height: 100% !important;">
+				<div class="slider-wrap" style="height: inherit !important;">
+					<div class="slide full-screen force-full-screen" style="background: url('images/aerial1.jpeg') center right; background-size: cover; height: 100% !important;"></div>
+					<div class="slide full-screen force-full-screen" style="background: url('images/aerial2.jpeg') center right; background-size: cover; height: 100% !important;"></div>
+					<div class="slide full-screen force-full-screen" style="background: url('images/aerial3.jpeg') center right; background-size: cover; height: 100% !important;"></div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<!-- Document Wrapper
+	============================================= -->
+	<div id="wrapper" class="clearfix">
+
+		<!-- Header
+		============================================= -->
+		<header id="header" class="nobottomborder no-sticky">
+
+			<div id="header-wrap">
+
+				<div class="container-fluid clearfix">
+
+					<div id="primary-menu-trigger"><i class="icon-reorder"></i></div>
+
+					<!-- Logo
+					============================================= -->
+					<div id="logo">
+						<a href="index.php" class="standard-logo" data-dark-logo="demos/media-agency/images/logo-media-dark.png"><img src="images/logo.png" alt="Canvas Logo"></a>
+						</div><!-- #logo end -->
+
+					<!-- Primary Navigation
+					============================================= -->
+					<nav id="primary-menu" class="style-2">
+
+						<ul>
+							<li><a href="#" data-scrollto="#section-about" data-easing="easeInOutExpo" data-speed="1250" data-offset="200"><div>About</div></a></li>
+							<li><a href="#" data-scrollto="#section-services" data-easing="easeInOutExpo" data-speed="1250" data-offset="60"><div>Services</div></a></li>
+							<li><a href="#" data-scrollto="#section-drones" data-easing="easeInOutExpo" data-speed="1250" data-offset="60"><div>Our Drones</div></a></li>
+							<li><a href="#" data-scrollto="#section-contact" data-easing="easeInOutExpo" data-speed="1250" data-offset="60"><div>Contact</div></a></li>
+						</ul>
+
+					</nav><!-- #primary-menu end -->
+
+				</div>
+
+			</div>
+
+		</header><!-- #header end -->
+
+		<div class="clear"></div>
+
+		<!-- Content
+		============================================= -->
+		<section id="content">
+
+			<div class="content-wrap">
+
+				<div id="section-about" class="container-fluid page-section clearfix">
+
+					<div class="hero-headline bottommargin">
+						<h2>We are <b class="customfont">Ascent UAV Limited</b>,<br>Professional Aerial Imagery,<br>Based in Wairarapa.</h2>
+						<span>Ascent UAV Ltd Currently operate the two latest UAV (drone) aircraft from both DJI and 3D Robitics 
+							giving great versatility and mission readiness.
+							<br>
+							Ascent UAV Ltd specialises in the following: Residential, Real-Estate and Property Marketing, Rural, Conservation,
+							Building / Roof Inspections, Insurance / Damage Reporting, Job Site Inspection
+						</span>
+					</div>
+
+					<a href="#" data-scrollto="#section-contact" data-easing="easeInOutExpo" data-speed="1250" data-offset="60" class="button button-dark button-black nomargin">Get Custom Quote</a>
+
+					<div class="line topmargin-lg bottommargin-lg"></div>
+
+					<div id="section-services" class="page-section">
+
+						<h2 class="bottommargin">Services.</h2>
+
+						<div class="row clearfix">
+
+							<div class="col-md-6 bottommargin">
+								<div class="feature-box fbox-plain fbox-dark">
+									<div class="fbox-icon">
+										<a href="#"><i class="icon-et-focus"></i></a>
+									</div>
+									<h3>Video and Still Images</h3>
+									<p>Mix and match as you wish</p>
+								</div>
+							</div>
+							<div class="col-md-6 bottommargin">
+								<div class="feature-box fbox-plain fbox-dark">
+									<div class="fbox-icon">
+										<a href="#"><i class="icon-et-pictures"></i></a>
+									</div>
+									<h3>Multiple Formats</h3>
+									<p>.</p>
+								</div>
+							</div>
+							<div class="col-md-6 bottommargin">
+								<div class="feature-box fbox-plain fbox-dark">
+									<div class="fbox-icon">
+										<a href="#"><i class="icon-et-shield"></i></a>
+									</div>
+									<h3>Your personal privacy respected and guaranteed</h3>
+									<p>.</p>
+								</div>
+							</div>
+							<div class="col-md-6 bottommargin">
+								<div class="feature-box fbox-plain fbox-dark">
+									<div class="fbox-icon">
+										<a href="#"><i class="icon-et-target"></i></a>
+									</div>
+									<h3>Fast Turnaround</h3>
+									<p>.</p>
+								</div>
+							</div>
+
+							<div class="clear bottommargin-sm"></div>
+
+							<div class="col-md-7">
+
+								<div class="accordion accordion-border clearfix">
+
+									<div class="acctitle"><i class="acc-closed icon-ok-circle"></i><i class="acc-open icon-remove-circle"></i>Our Mission</div>
+									<div class="acc_content clearfix">Donec sed odio dui. Nulla vitae elit libero, a pharetra augue. Nullam id dolor id nibh ultricies vehicula ut id elit. Integer posuere erat a ante venenatis dapibus posuere velit aliquet.</div>
+
+									<div class="acctitle"><i class="acc-closed icon-ok-circle"></i><i class="acc-open icon-remove-circle"></i>What we Do?</div>
+									<div class="acc_content clearfix">Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Duis mollis, est non commodo luctus. Aenean lacinia bibendum nulla sed consectetur. Cras mattis consectetur purus sit amet fermentum.</div>
+
+									<div class="acctitle"><i class="acc-closed icon-ok-circle"></i><i class="acc-open icon-remove-circle"></i>Our Company's Values</div>
+									<div class="acc_content clearfix">Nullam id dolor id nibh ultricies vehicula ut id elit. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Duis mollis, est non commodo luctus. Aenean lacinia bibendum nulla sed consectetur.</div>
+
+								</div>
+
+							</div>
+
+							<div class="col-md-5">
+
+								<h4>Clients Say.</h4>
+
+								<div class="fslider testimonial nopadding noborder noshadow" data-animation="slide" data-arrows="false">
+									<div class="flexslider">
+										<div class="slider-wrap">
+											<div class="slide">
+												<div class="testi-content">
+													<p>Similique fugit repellendus expedita excepturi iure perferendis provident quia eaque. Repellendus, vero numquam?</p>
+													<div class="testi-meta">
+														Steve Jobs
+														<span>Apple Inc.</span>
+													</div>
+												</div>
+											</div>
+											<div class="slide">
+												<div class="testi-content">
+													<p>Natus voluptatum enim quod necessitatibus quis expedita harum provident eos obcaecati id culpa corporis molestias.</p>
+													<div class="testi-meta">
+														Collis Ta'eed
+														<span>Envato Inc.</span>
+													</div>
+												</div>
+											</div>
+											<div class="slide">
+												<div class="testi-content">
+													<p>Incidunt deleniti blanditiis quas aperiam recusandae consequatur ullam quibusdam cum libero illo rerum!</p>
+													<div class="testi-meta">
+														John Doe
+														<span>XYZ Inc.</span>
+													</div>
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
+
+							</div>
+
+						</div>
+
+					</div>
+
+					<div class="line topmargin-lg bottommargin-lg"></div>
+
+					<div id="section-drones" class="page-section">
+
+						<h2 class="bottommargin">Our Drones.</h2>
+
+						<!-- Drones Items
+						============================================= -->
+						<div id="portfolio" class="portfolio grid-container portfolio-nomargin portfolio-full portfolio-3 portfolio-masonry mixed-masonry clearfix">
+
+							<article class="">
+								<div class="portfolio-image imagescale">
+									<a href="#">
+										<img src="images/drone2.jpg" alt="3DR Solo" title="3DR Solo" data-toggle="tooltip" data-placement="top">
+									</a>
+									<div class="on-hover" style="background: rgba(255,255,255,0.7);">
+										
+									</div>
+								</div>
+								<div class="portfolio-desc">
+									<h3><a href="#">3DR Solo</a></h3>
+									<span>Solo packs over 35 sensors and 20 microprocessors into a rigid, 
+										lightweight unibody shell. All to bring you the easiest to fly and 
+										most capable aerial drone on the market.
+										The modular design allows you to quickly replace motors or change 
+										camera gimbals with a simple screw driver 
+										making Solo easy to repair and future-proof.
+ 										<a href="https://3dr.com/solo-drone/specs/" title="More Info"><i class="icon-line-plus"></i></a>
+ 										<br>
+ 									</span>
+								</div>
+							</article>
+
+							<article class="">
+								<div class="portfolio-image imagescale">
+									<a href="#">
+										<img src="images/drone3.jpg" alt="DJI Phantom 4" title="DJI Phantom 4" data-toggle="tooltip" data-placement="top">
+									</a>
+									<div class="on-hover" style="background: rgba(255,255,255,0.7);">
+										<!-- <a href="#"><i class="icon-line-plus"></i></a> -->
+									</div>
+								</div>
+								<div class="portfolio-desc">
+									<h3><a href="#">DJI Phantom 4</a></h3>
+									<span>Shoot sharp, clean video in up to 4K at 30fps and Full HD 1080p at 120fps for slow motion, 
+										through a newly designed lens that dramatically increases sharpness.
+										A range of video color profiles are available to help you get the look you want easily,
+										 from bright vibrant colors to professional flat profiles including D-Log and Cine-D that are 
+										 ready for video post-production. Photographers can shoot images in 12 megapixel Adobe DNG RAW.
+										<a href="http://www.dji.com/phantom-4/info#specs" title="More Info"><i class="icon-line-plus"></i></a>				
+									</span>
+								</div>
+							</article>
+
+							
+
+						</div><!-- #drones end -->
+
+		
+
+					<div class="line topmargin-lg bottommargin-lg"></div>
+
+					<div id="section-contact" class="page-section">
+
+					<h2 class="bottommargin">Get in Touch.</h2>
+
+						<div class="row clearfix">
+
+							<div class="col-md-12">
+
+								<div class="contact-widget">
+
+									<div class="contact-form-result"></div>
+
+									<form class="nobottommargin" id="template-contactform" name="template-contactform" action="include/sendemail.php" method="post">
+
+										<div class="form-process"></div>
+
+										<div class="col_half">
+											<input type="text" id="template-contactform-name" name="template-contactform-name" value="" class="sm-form-control border-form-control required" placeholder="Name" />
+										</div>
+										<div class="col_half col_last">
+											<input type="email" id="template-contactform-email" name="template-contactform-email" value="" class="required email sm-form-control border-form-control" placeholder="Email Address" />
+										</div>
+
+										<div class="clear"></div>
+
+										<div class="col_full">
+											<input type="text" id="template-contactform-subject" name="template-contactform-subject" value="" class="required sm-form-control border-form-control" placeholder="Subject" />
+										</div>
+
+										<div class="col_full">
+											<textarea class="required sm-form-control border-form-control" id="template-contactform-message" name="template-contactform-message" rows="7" cols="30" placeholder="Your Message"></textarea>
+										</div>
+
+										<div class="col_full">
+											<button class="button button-black noleftmargin topmargin-sm" type="submit" id="template-contactform-submit" name="template-contactform-submit" value="submit">Send Message</button>
+										</div>
+
+										<div class="clear"></div>
+
+										<div class="col_full hidden">
+											<input type="text" id="template-contactform-botcheck" name="template-contactform-botcheck" value="" class="sm-form-control" />
+										</div>
+
+									</form>
+
+								</div>
+														</div>
+
+							<div class="col-md-6">
+								<h4>Headquarters.</h4>
+
+								<div style="font-size: 16px; line-height: 1.7;">
+									<address style="line-height: 1.7;">
+										50 Fitzherbert Street<br>
+										Featherston, 5710<br>
+									</address>
+
+									<div class="clear topmargin"></div>
+
+									<abbr title="Phone Number"><strong>Phone:</strong></abbr> 022 164 8464<br>
+									<abbr title="Email Address"><strong>Email:</strong></abbr> ascent.uav@gmail.com<br>
+									<abbr title="Opening Hours"><strong>Opening Hours:</strong><br>
+									</abbr>Mon - Thu: 10:00 - 14:00, 17:00 - 21:00<br>
+									</abbr>Fri - Sun: 11:00 - 23:00<br>
+								</div>
+							</div>
+							<div class="col-md-6">
+								<h4>Map.</h4>
+
+								<div id="map" style="height: 250px; margin-bottom: 20px;" class="gmap"></div>
+
+							</div>
+							<div class="col-md-10">
+								<h4>Payment Methods.</h4>
+								<div class="col-md-2">
+									<img src="images/visa.png" alt="VISA" title="VISA" data-toggle="tooltip" data-placement="top"/>
+								</div>
+								<div class="col-md-2">
+									<img src="images/mastercard.png" alt="Master Card" title="Master Card" data-toggle="tooltip" data-placement="top" />
+								</div>
+								<div class="col-md-2">
+									<img src="images/amex.png" alt="American Express" title="American Express" data-toggle="tooltip" data-placement="top"/>
+								</div>
+								<div class="col-md-2">
+									<img src="images/credit.png" alt="Debit" title="Debit" data-toggle="tooltip" data-placement="top"/>
+								</div>
+								<div class="col-md-2">
+									<img src="images/money.png" alt="Cash" title="Cash" data-toggle="tooltip" data-placement="top"/>
+								</div>
+							</div>
+
+							</div>
+
+
+						</div>
+
+					</div>
+
+				</div>
+
+			</div>
+
+		</section><!-- #content end -->
+
+		<!-- Footer
+		============================================= -->
+		<footer id="footer" class="dark">
+			<div id="copyrights" style="background-color:#111;">
+				<div class="container-fluid">
+
+					Copyrights &copy; Canvas 2015 | All Rights Reserved <br>
+					Implemented by <a href="http://www.miguel.as">Miguel Araya</a>
+
+				</div>
+			</div>
+		</footer><!-- #footer end -->
+
+	</div><!-- #wrapper end -->
+
+	<!-- External JavaScripts
+	============================================= -->
+	<script type="text/javascript" src="js/jquery.js"></script>
+	<script type="text/javascript" src="js/plugins.js"></script>
+	<script type="text/javascript" src="js/jquery.gmap.js"></script>
+	<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDIO7v3i7ZH-X1Q4nsGE6QRFWq2yydkLmE"></script>
+
+
+
+
+	<!-- Footer Scripts
+	============================================= -->
+	<script type="text/javascript" src="js/functions.js"></script>
+
+	<script type="text/javascript">
+
+		$('#map').gMap({
+
+			 address: '50 fitzherbert Street Featherston',
+			 maptype: 'ROADMAP',
+			 zoom: 15,
+			 markers: [
+				{
+					address: "50 fitzherbert Street Featherston"
+				}
+			 ],
+			 doubleclickzoom: false,
+			 controls: {
+				 panControl: true,
+				 zoomControl: true,
+				 mapTypeControl: true,
+				 scaleControl: false,
+				 streetViewControl: false,
+				 overviewMapControl: false
+			}
+		});
+		</script>
+
+</body>
+</html>
